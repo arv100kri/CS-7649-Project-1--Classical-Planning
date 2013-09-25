15b63f14a435dd2bff58c2869dd4d985e3808e33
arv100kri <arv100.kri@gmail.com>
2013-09-25 00:54:10 -0400

    Added Stango's modified Planner. Plans are now readable

--- src/problem4/Normalized Sokoban Planner.cpp
+++ src/problem4/Normalized Sokoban Planner.cpp
@@ -17,22 +17,23 @@
 
 using namespace std;
 
-typedef		long long LL;
-typedef		vector<LL> VLL;
+typedef	long long LL;
+typedef	vector<LL> VLL;
 
 vector <string> Graph, CleanGraph;
-int				Row, Column, Boxes;
+int	Row, Column, Boxes;
 map <LL, int>	StateNo;
-int				MaximumPower;
+int	MaximumPower;
 vector <VLL>	State;
 vector <int>	opt;
-LL				inital, target;
+vector <LL>	previousState;
+LL	inital, target;
 vector <int>	Lst;
-int				targetrange;
-bool			getAns;
-int				res = -1, dimen;
+int	targetrange, AnsID;
+bool getAns;
+int	res = -1, dimen;
 
-void	ParseState(int sta[], LL s)
+void ParseState(int sta[], LL s)
 {
 	for (int i = dimen - 1; i >= 0; i --)
 	{
@@ -42,112 +43,124 @@ void	ParseState(int sta[], LL s)
 	}
 }
 
-int		getStateID(LL sta) //no match found return -1
+int	getStateID(LL sta) //no match found return -1
 {
 	map<LL,int>::iterator it = StateNo.find(sta);
 	if (it != StateNo.end()) return it->second;
 	else return -1;
 }
 
-int		AddStateComponent (LL sta)
+int	AddStateComponent (LL sta)
 {
-	int		IDD = getStateID(sta);
+	int	IDD = getStateID(sta);
 	if (getStateID(sta) != -1) return IDD;
-	int		staid = -1;
-	int		a[dimen];
+	int	staid = -1;
+	int	a[dimen];
 	ParseState(a, sta);
+
+	/*if (sta == 140800827524) {
     for (int i = 0; i < dimen; i ++) cout << a[i] << " ";
     cout << endl;
+	}*/
 
-	vector <LL>		statecomponent;
-	LL	boxsta = 0;
-	vector <string>	G(CleanGraph);
-	for (int i = 0; i < Boxes; i ++)
+vector <LL>	statecomponent;
+LL	boxsta = 0;
+vector <string>	G(CleanGraph);
+for (int i = 0; i < Boxes; i ++)
+{
+	G[a[2 * i]][a[2 * i + 1]] = 'B';
+		//if (sta == 140800827524) cout << a[2 * i] << " " << a[2 * i + 1] << endl;
+	boxsta <<= MaximumPower;
+	boxsta |= a[2 * i];
+	boxsta <<= MaximumPower;
+	boxsta |= a[2 * i + 1];
+}
+boxsta <<= (2 * MaximumPower);
+
+int	stx = a[dimen - 2], sty = a[dimen - 1];
+bool reach[Row][Column];
+memset(reach, 0, sizeof(reach));
+reach[stx][sty] = true;
+vector <int>	L;
+L.push_back(stx); L.push_back(sty);
+for (int head = 0; head < L.size(); head += 2)
+{
+	int	curx = L[head], cury = L[head + 1];
+	int	ax, bx, ay, by;
+	ax = (curx > 0)?curx - 1:curx;
+	bx = (curx < Row - 1)?curx + 1:curx;
+	ay = (cury > 0)?cury - 1:cury;
+	by = (cury < Column - 1)?cury + 1:cury;
+	if (G[ax][cury] == '*' && !reach[ax][cury])
 	{
-		G[a[2 * i]][a[2 * i + 1]] = 'B';
-		boxsta <<= MaximumPower;
-		boxsta |= a[2 * i];
-		boxsta <<= MaximumPower;
-		boxsta |= a[2 * i + 1];
+		L.push_back(ax); L.push_back(cury);
+		reach[ax][cury] = true;
 	}
-	boxsta <<= (2 * MaximumPower);
-
-	int	stx = a[dimen - 2], sty = a[dimen - 1];
-	bool	reach[Row][Column];
-	memset(reach, 0, sizeof(reach));
-	reach[stx][sty] = true;
-	vector <int>	L;
-	L.push_back(stx); L.push_back(sty);
-	for (int head = 0; head < L.size(); head += 2)
+	if (G[bx][cury] == '*' && !reach[bx][cury])
 	{
-		int	curx = L[head], cury = L[head + 1];
-		int	ax, bx, ay, by;
-		ax = (curx > 0)?curx - 1:curx;
-		bx = (curx < Row - 1)?curx + 1:curx;
-		ay = (cury > 0)?cury - 1:cury;
-		by = (cury < Column - 1)?cury + 1:cury;
-		if (G[ax][cury] == '*' && !reach[ax][cury])
-		{
-			L.push_back(ax); L.push_back(cury);
-			reach[ax][cury] = true;
-		}
-		if (G[bx][cury] == '*' && !reach[bx][cury])
-		{
-			L.push_back(bx); L.push_back(cury);
-			reach[bx][cury] = true;
-		}
-		if (G[curx][ay] == '*' && !reach[curx][ay])
-		{
-			L.push_back(curx); L.push_back(ay);
-			reach[curx][ay] = true;
-		}
-		if (G[curx][by] == '*' && !reach[curx][by])
-		{
-			L.push_back(curx); L.push_back(by);
-			reach[curx][by] = true;
-		}
+		L.push_back(bx); L.push_back(cury);
+		reach[bx][cury] = true;
 	}
-
-	for (int i = 0; i < Row; i ++)
-		for (int j = 0; j < Column; j ++)
-			if (reach[i][j])
-			{
-				LL	tmp = boxsta;
-				tmp |= ((i << MaximumPower) | j);
-				statecomponent.push_back(tmp);
-			}
-
-	//Save new State
-	State.push_back(statecomponent);
-	opt.push_back(-1);
-	int		id = State.size() - 1;
-	for (int i = 0; i < statecomponent.size(); i ++)
-		StateNo[statecomponent[i]] = id;
-
-	return id;
+	if (G[curx][ay] == '*' && !reach[curx][ay])
+	{
+		L.push_back(curx); L.push_back(ay);
+		reach[curx][ay] = true;
+	}
+	if (G[curx][by] == '*' && !reach[curx][by])
+	{
+		L.push_back(curx); L.push_back(by);
+		reach[curx][by] = true;
+	}
 }
 
+for (int i = 0; i < Row; i ++)
+	for (int j = 0; j < Column; j ++)
+		if (reach[i][j])
+		{
+			LL	tmp = boxsta;
+			tmp |= ((i << MaximumPower) | j);
+			statecomponent.push_back(tmp);
+				/*
+				if (sta == 140800827524)
+					{
+						ParseState(a, tmp); for (int i = 0; i < dimen; i ++) cout << a[i] << " ";
+					    cout << endl;
+					}
+				*/
+				}
 
-void	init(char *input, char *output)
+		//Save new State
+			State.push_back(statecomponent);
+			opt.push_back(-1);
+			previousState.push_back(-1);
+			int		id = State.size() - 1;
+			for (int i = 0; i < statecomponent.size(); i ++)
+				StateNo[statecomponent[i]] = id;
+
+			return id;
+		}
+
+
+void init(char *input, char *output)
 {
 	freopen (input, "r", stdin);
 	freopen (output, "w", stdout);
 
 	cin >> Row >> Column >> Boxes;
-    dimen = Boxes * 2 + 2;
+	dimen = Boxes * 2 + 2;
 	for (int i = 0; i < Row; i ++) {
 		string	tmp;
-		cin >> tmp;
+		cin >> tmp; 
 		Graph.push_back(tmp);
 	}
 
-	// Count MaximunPower (for state the whole map)
-	int		tmp = max(Row, Column), tmp2 = 1;
+// Count MaximunPower (for state the whole map)
+	int	tmp = max(Row, Column), tmp2 = 1;
 	MaximumPower = 1;
 	for (; tmp2 < tmp; MaximumPower ++, tmp2 *= 2);
-    //cout << MaximumPower << endl;
+//cout << MaximumPower << endl;
 
-	// Get the inital state and target state
+// Get the inital state and target state
 
 	inital = 0; target = 0;
 
@@ -185,9 +198,10 @@ void	init(char *input, char *output)
 	inital |= PersonX;
 	inital <<= MaximumPower;
 	inital |= PersonY;
-	cout << "Inital" << endl;
+//cout << "Inital" << endl;
 	AddStateComponent(inital);
 	opt[0] = 0;
+	previousState[0] = 0;
 
 	LL tmptar = target;
 
@@ -198,18 +212,18 @@ void	init(char *input, char *output)
 				target |= i;
 				target <<= MaximumPower;
 				target |= j;
-				cout << "Target" << endl;
+	//cout << "Target" << endl;
 				int		pp = AddStateComponent (target);
 				target = tmptar;
 			}
 
-	targetrange = State.size();
-}
+			targetrange = State.size();
+		}
 
 
 void	bfs()
 {
-	cout << "Search Begin" << endl;
+//cout << "Search Begin" << endl;
 	getAns = false;
 	res = -1;
 
@@ -218,15 +232,14 @@ void	bfs()
 
 	for (int head = 0; head < Lst.size(); head ++)
 	{
-		int		curnode = Lst[head];
-		cout << "Node : " << curnode << endl;
-		int		sz = State[curnode].size();
+		int	curnode = Lst[head];
+//cout << "Node : " << curnode << endl;
+		int	sz = State[curnode].size();
 
 		vector <string>	G(CleanGraph);
-		int		a[dimen];
+		int	a[dimen];
 		ParseState(a, State[curnode][0]);
 		for (int i = 0; i < Boxes; i ++) G[a[2 * i]][a[2 * i + 1]] = 'B';
-
 		for (int i = 0; i < sz; i ++)
 		{
 			int		curstate = State[curnode][i];
@@ -239,63 +252,118 @@ void	bfs()
 			int		direc[4][2] = {{1, 0},{0, 1},{-1, 0},{0, -1}};
 			for (int j = 0; j < 4; j ++)
 				if (G[PersonX + direc[j][0]][PersonY + direc[j][1]] == 'B' &&
-                    G[PersonX + 2 * direc[j][0]][PersonY + 2 * direc[j][1]] == '*')
+					G[PersonX + 2 * direc[j][0]][PersonY + 2 * direc[j][1]] == '*')
 				{
 					G[PersonX + 2 * direc[j][0]][PersonY + 2 * direc[j][1]] = 'B';
 					G[PersonX + direc[j][0]][PersonY + direc[j][1]] = 'M';
 					G[PersonX][PersonY] = '*';
 					LL	nextsta = 0;
-					int		pp, qq;
+					int	pp, qq;
 					for (int lv1 = 0; lv1 < Row; lv1 ++)
 						for (int lv2 = 0; lv2 < Column; lv2 ++)
-                            if (G[lv1][lv2] == 'B')
-                            {
-                                nextsta <<= MaximumPower;
-                                nextsta |= lv1;
-                                nextsta <<= MaximumPower;
-                                nextsta |= lv2;
-                            } else if (G[lv1][lv2] == 'M') {
-                                pp = lv1;
-                                qq = lv2;
-                            }
-					nextsta <<= MaximumPower;
-					nextsta |= pp;
-					nextsta <<= MaximumPower;
-					nextsta |= qq;
-					cout << "Next Sta" << endl;
-					int		nextid = getStateID(nextsta);
-					if (nextid < targetrange && nextid > 0)
-					{
-						cout << "Accomplished: " << nextid << endl;
-						opt[nextid] = opt[curnode] + 1;
-						res = opt[nextid];
-						getAns = true;
-						return;
-					}
-					else if (nextid == -1)
-					{
-						int		newid = AddStateComponent(nextsta);
-						cout << "New State ID: " << newid << endl;
-						opt[newid] = opt[curnode] + 1;
-						Lst.push_back(newid);
-					}
-					G[PersonX + 2 * direc[j][0]][PersonY + 2 * direc[j][1]] = '*';
-					G[PersonX + direc[j][0]][PersonY + direc[j][1]] = 'B';
-					G[PersonX][PersonY] = '*';
+							if (G[lv1][lv2] == 'B')
+							{
+								nextsta <<= MaximumPower;
+								nextsta |= lv1;
+								nextsta <<= MaximumPower;
+								nextsta |= lv2;
+							} else if (G[lv1][lv2] == 'M') {
+								pp = lv1;
+								qq = lv2;
+							}
+							nextsta <<= MaximumPower;
+							nextsta |= pp;
+							nextsta <<= MaximumPower;
+							nextsta |= qq;
+
+
+
+//cout << "Next Sta" << endl;
+							int	nextid = getStateID(nextsta);
+							if (nextid < targetrange && nextid > 0)
+							{
+//cout << "Accomplished: " << nextid << endl;
+								opt[nextid] = opt[curnode] + 1;
+								previousState[nextid] = curnode;
+								res = opt[nextid];
+								AnsID = nextid;
+								getAns = true;
+								return;
+							}
+							else if (nextid == -1)
+							{
+//cout << PersonX << " " << PersonY << " " << curnode << endl;
+								int		newid = AddStateComponent(nextsta);
+								if (newid == 4)
+								{
+									ParseState(a, nextsta);
+	//for (int i = 0; i < dimen; i ++) cout << a[i] << " ";
+	//cout << endl;
+	//cout << "---" << nextsta << endl;
+								}
+								previousState[previousState.size() - 1] = curnode;
+//cout << "New State ID: " << newid << endl;
+								opt[newid] = opt[curnode] + 1;
+//if (newid == 342) cout << Lst.
+								Lst.push_back(newid);
+
+							}
+							G[PersonX + 2 * direc[j][0]][PersonY + 2 * direc[j][1]] = '*';
+							G[PersonX + direc[j][0]][PersonY + direc[j][1]] = 'B';
+							G[PersonX][PersonY] = '*';
 				}
 		}
 	}
 }
 
+void printState(int ID, int& step)
+{
+//cout << ID << endl;
+	if (ID == 0) {
+		cout << "Step : " << step << endl;
+		step ++;
+		for (int i = 0; i < Row; i ++) cout << Graph[i] << endl;
 
-int		main(int argc, char **argv)
+			return;
+	}
+	printState(previousState[ID], step);
+	cout << "Step : " << step << endl;
+//cout << ID << endl;
+	int	curnode = ID;
+	LL	curstate = State[ID][0];
+	vector <string>	G(CleanGraph);
+	int	a[dimen];
+	ParseState(a, curstate);
+//for (int i = 0; i < dimen; i ++) cout << a[i] << " ";
+//cout << endl;
+	for (int i = 0; i < Boxes; i ++) {
+//cout << a[2 * i] << " " << a[2 * i + 1] << endl;
+		G[a[2 * i]][a[2 * i + 1]] = 'B';
+	}
+	for (int i = 0; i < Row; i ++) cout << G[i] << endl;
+		step ++;
+	return;
+}
+
+
+void printAns()
+{
+	if (!getAns) { cout << "No Solution!" << endl; return; }
+	cout << "Total Step : " << res << endl;
+	int		sp = 0;
+	printState(AnsID, sp);
+}
+
+int	main(int argc, char **argv)
 {
 	if (argc < 3) {
 		cout<<"Usage: sokoban <input_file> <output_file>"<<endl;
 		exit(1);
 	}
-	init (argv[1], argv[2]);
-	bfs  ();
-	cout << "ans : " << res << endl;
+	init(argv[1], argv[2]);
+	bfs();
+//cout << "ans : " << res << endl;
+
+	printAns();
 	return 0;
 }
